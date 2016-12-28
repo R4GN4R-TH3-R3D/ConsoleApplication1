753e957af4c70463ab3bf3614f75824572ae7f44
R4GN4R-TH3-R3D <Ryan.s.spellins@live.com>
2016-12-27 21:20:43 -0500

    First Commit
    Basic Class Library that contains a class which creates hello world
    text.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textBuilder = new HelloWorldTextBuilder();
+
+            var text = textBuilder.CreateHelloWorldText();
+
+            Console.WriteLine(text);            
+        }
+    }
+}
